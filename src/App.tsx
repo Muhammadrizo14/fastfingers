72d9e8a57de1f11aa6a26d553d623d2fd5c2f285
Muhammadrizo <mtursunboev13@gmail.com>
2024-01-15 03:00:26 +0500

    clean code

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
   const [isRunning, setIsRunning] = useState(false);
   const timerRef = useRef<number | any>();
   const input = useRef<any>()
-  const [numberOfWords, setNumberOfWords] = useState(5)
+  const [numberOfWords, setNumberOfWords] = useState(2)
 
   const start = () => {
     if (isRunning) {
@@ -39,9 +39,13 @@ function App() {
 
 
   const tryAgain = () => {
+    setWords([])
     setModal(false)
     getAllWords()
+    setCounter(1)
     setPassedWords(0)
+    input.current.focus()
+
   }
 
   const formatTime = (milliseconds: any) => {
@@ -55,12 +59,6 @@ function App() {
   };
 
   const updateItem = async (itemId: number, status: string) => {
-
-
-    console.log(`came id ${itemId} to ${status}`);
-    ;
-
-    // Create a new array with the updated item
     const updatedItems = words.map(item => {
       if (item.id === itemId) {
         if (words.length !== itemId) {
@@ -72,7 +70,6 @@ function App() {
       }
     });
 
-    // Set the new state
     setWords(updatedItems);
   };
 
@@ -100,30 +97,20 @@ function App() {
 
 
 
-  const checkInput = async (e: React.ChangeEvent<HTMLInputElement>) => {
+  const checkInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTypeInput(e.target.value)
     !isRunning && start()
 
 
     if (e.target.value.includes(' ')) {
-      console.log(`must be ${words[counter - 1].word}`);
-      console.log(`typed ${typeInput}`);
-      console.log(`counter ${counter}`);
-
-
       if (typeInput === words[counter - 1].word) {
-        console.log(`success`);
-
-        await updateItem(counter, 'passed')
-        await setPassedWords(prev => prev + 1)
+        updateItem(counter, 'passed')
+        setPassedWords(prev => prev + 1)
         setCounter(prev => prev + 1)
       } else {
-        console.log(`error`);
-        await updateItem(counter, 'err')
+        updateItem(counter, 'err')
         setCounter(prev => prev + 1)
       }
-
-
       setTypeInput('')
 
       if (counter === words.length) {
@@ -131,9 +118,7 @@ function App() {
         setModal(true)
 
         const seconds = time / 1000;
-
-
-        const minutes = seconds / 60;
+        const minutes = seconds / 60; // or ms to 60000
 
         setWpm((passedWords + 1 / 5) / minutes)
       }
@@ -141,9 +126,6 @@ function App() {
   }
 
 
-  useEffect(() => {
-    console.log('Updated State:', words);
-  }, [words]);
   useEffect(() => {
     input.current.focus()
     getAllWords()
@@ -157,7 +139,6 @@ function App() {
       </h1>
 
 
-
       <button className='settingBtn' onClick={() => setSettingsModal(true)}>
         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M12 8.666c-1.838 0-3.333 1.496-3.333 3.334s1.495 3.333 3.333 3.333 3.333-1.495 3.333-3.333-1.495-3.334-3.333-3.334m0 7.667c-2.39 0-4.333-1.943-4.333-4.333s1.943-4.334 4.333-4.334 4.333 1.944 4.333 4.334c0 2.39-1.943 4.333-4.333 4.333m-1.193 6.667h2.386c.379-1.104.668-2.451 2.107-3.05 1.496-.617 2.666.196 3.635.672l1.686-1.688c-.508-1.047-1.266-2.199-.669-3.641.567-1.369 1.739-1.663 3.048-2.099v-2.388c-1.235-.421-2.471-.708-3.047-2.098-.572-1.38.057-2.395.669-3.643l-1.687-1.686c-1.117.547-2.221 1.257-3.642.668-1.374-.571-1.656-1.734-2.1-3.047h-2.386c-.424 1.231-.704 2.468-2.099 3.046-.365.153-.718.226-1.077.226-.843 0-1.539-.392-2.566-.893l-1.687 1.686c.574 1.175 1.251 2.237.669 3.643-.571 1.375-1.734 1.654-3.047 2.098v2.388c1.226.418 2.468.705 3.047 2.098.581 1.403-.075 2.432-.669 3.643l1.687 1.687c1.45-.725 2.355-1.204 3.642-.669 1.378.572 1.655 1.738 2.1 3.047m3.094 1h-3.803c-.681-1.918-.785-2.713-1.773-3.123-1.005-.419-1.731.132-3.466.952l-2.689-2.689c.873-1.837 1.367-2.465.953-3.465-.412-.991-1.192-1.087-3.123-1.773v-3.804c1.906-.678 2.712-.782 3.123-1.773.411-.991-.071-1.613-.953-3.466l2.689-2.688c1.741.828 2.466 1.365 3.465.953.992-.412 1.082-1.185 1.775-3.124h3.802c.682 1.918.788 2.714 1.774 3.123 1.001.416 1.709-.119 3.467-.952l2.687 2.688c-.878 1.847-1.361 2.477-.952 3.465.411.992 1.192 1.087 3.123 1.774v3.805c-1.906.677-2.713.782-3.124 1.773-.403.975.044 1.561.954 3.464l-2.688 2.689c-1.728-.82-2.467-1.37-3.456-.955-.988.41-1.08 1.146-1.785 3.126" /></svg>
       </button>
@@ -167,7 +148,6 @@ function App() {
         <Popup>
           <>
             <h1>Settings</h1>
-
 
             <input type="number" value={numberOfWords} min={1} onChange={(e: any) => { setNumberOfWords(e.target.value) }} placeholder='Length of words' />
 
