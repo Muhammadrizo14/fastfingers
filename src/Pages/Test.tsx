e1130848ce418fb989ee7f1691da53c5ab62df74
Muhammadrizo <mtursunboev13@gmail.com>
2024-02-03 21:11:36 +0500

    localStorage has already added

--- src/Pages/Test.tsx
+++ src/Pages/Test.tsx
@@ -1,35 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
+// Хуки находятся в react-redux
+import { useSelector, useDispatch } from 'react-redux';
+// Импортируем нужные действия
+import { decrement, increment, incrementByAmount } from '../slices/counterSlice.js';
 
-const MyComponent = () => {
-    const [items, setItems] = useState([
-        { id: 1, name: 'Item 1' },
-        { id: 2, name: 'Item 2' },
-        { id: 3, name: 'Item 3' },
-    ]);
+export default () => {
+  // Вытаскиваем данные из хранилища
+  // Здесь state — это все состояние
+  const count = useSelector((state:any) => state.counter.value);
+  // Возвращает метод store.dispatch() текущего хранилища
+  const dispatch = useDispatch();
 
-    // Function to update the state
-    const updateItem = (itemId: any, newName: any) => {
-        // Create a new array with the updated item
-        const updatedItems = items.map(item =>
-            item.id === itemId ? { ...item, name: newName } : item
-        );
-
-        // Set the new state
-        setItems(updatedItems);
-    };
-
-    return (
-        <div>
-            {items.map(item => (
-                <div key={item.id}>
-                    {item.name}
-                    <button onClick={() => updateItem(item.id, `New ${item.name}`)}>
-                        Update
-                    </button>
-                </div>
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Прибавить
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Отнять
+        </button>
+        <br />
+        <button onClick={() => dispatch(incrementByAmount(42))}>Прибавить 42</button>
+      </div>
+    </div>
+  );
 };
-
-export default MyComponent;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import store from './slices/index.js';
 import reportWebVitals from './reportWebVitals';
 import {
   RouterProvider,
 } from "react-router-dom";
 import router from './router';
+import {Provider} from "react-redux";
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <Provider store={store}>
   <RouterProvider router={router} />
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
